e945c659ea3d3f41936afc32414bdab2d298a9d1
Hana Lee <i@leehana.co.kr>
2015-12-12 14:28:06 +0900

    Code cleanup

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -25,8 +25,8 @@ class RecipesController < ApplicationController
   end
 
   private
-    def recipe_params
-      params.require(:recipe).permit(:name, :summary, :description)
-    end
+  def recipe_params
+    params.require(:recipe).permit(:name, :summary, :description)
+  end
 
 end
